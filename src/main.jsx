5e3f91ab598c16b0a230b3a2fa55edb3120aae97
Yahia Abdeldjalil <ly_benyahia@esi.dz>
2023-10-27 19:07:32 +0100

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,18 @@ import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import LoggedNavbar from "./components/LoggedNavbar.jsx";
-
+import { AuthProvider } from "react-auth-kit";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <AuthProvider
+      authType={"cookie"}
+      authName={"_auth"}
+      cookieDomain={window.location.hostname}
+      cookieSecure={window.location.protocol === "https:"}
+    >
+      <Router>
+        <App />
+      </Router>
+    </AuthProvider>
   </React.StrictMode>
 );
